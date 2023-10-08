451fe3b845248a813e6bf68c33a2deab69ac4fd9
Debasis420 <28debasismishra@gmail.com>
2023-10-08 11:30:10 +0530

    Heads or Tails

---
+++ Mini Projects/Head_or_Tails.py
@@ -0,0 +1,11 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+x= random.randint(0, 1)
+
+if x == 1:
+  print("Heads")
+  
+else:
+  print("Tails")
